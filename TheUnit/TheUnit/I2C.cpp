e4ed792b67563272b2419c720db31fb772e48b5e
rchard2scout <rchard2scout@users.noreply.github.com>
2017-05-14 12:09:40 +0200

    Implement I2C communication
    Not tested, but it does compile

---
+++ TheUnit/TheUnit/I2C.cpp
@@ -0,0 +1,78 @@
+#include "I2C.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <cstring>
+#include <iostream>
+
+I2C::I2C(): currentTarget(0x00){
+	filedescriptor = open(FILENAME_I2C, O_RDWR);
+	if(filedescriptor < 0){
+		std::cerr << "Failed to open the i2c bus\n";
+		std::cerr << strerror(errno) << std::endl;
+	}
+}
+
+I2C::~I2C(){
+	close(filedescriptor);
+	// reset multiplexer?
+}
+
+bool I2C::writeByte(uint8_t address, uint8_t data){
+	if(address != currentTarget){
+		if(!_setSlave(address)){
+			std::cerr << "Did not try to write " << data << " to " << address << std::endl;
+			return false;
+		}
+	}
+	return _write(address, data);
+}
+
+bool I2C::_setSlave(uint8_t target){
+	int data = 1 << target; // create bitmask from target number
+	if(_write(MULTIPLEXER_ADDRESS, data)){
+		currentTarget = target;
+		return true;
+	}
+	std::cerr << "Failed to set slave to " << target << std::endl;
+	return false;
+}
+
+bool I2C::_write(uint8_t address, uint8_t data){
+	if((ioctl(filedescriptor, I2C_SLAVE, address)) < 0)
+	{
+		std::cerr << "Failed to acquire bus access and/or talk to slave.\n";
+		std::cerr << strerror(errno) << std::endl;
+		return false;
+	}
+	return _writeByte(data);
+}
+
+bool I2C::_writeByte(uint8_t data){
+	uint8_t buffer[2] = {data, 0};
+	int length = 1;
+	if(write(filedescriptor, buffer, length) != length)
+	{
+		std::cerr << "Failed to write to the i2c bus.\n";
+		std::cerr << strerror(errno) << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool I2C::read(uint8_t* buffer, int length){
+	uint8_t tmpbuffer[length + 1] = {0}
+	bool success = false;
+	while(!success){
+		if(read(filedescriptor, tmpbuffer, length) != length)
+		{
+			usleep(100);
+		}
+		else
+		{
+			success = true;
+		}
+	}
+	memcpy(buffer, tmpbuffer, length);
+}
--- TheUnit/TheUnit/I2C.h
+++ TheUnit/TheUnit/I2C.h
@@ -1,14 +1,25 @@
 #ifndef I2C_H
 #define I2C_H
 
-#include "iCommunication.h";
+#include "iCommunication.h"
+#include <cstdint>
+
+#define FILENAME_I2C "/dev/i2c-1"
+#define MULTIPLEXER_ADDRESS 0x70 //depends on A2:0 pins
 
 class I2C : public iCommunication
 {
+public:
 	I2C();
 	virtual ~I2C();
+	bool writeByte(uint8_t address, uint8_t data); //return true on succes
+	bool read(uint8_t* buffer, int length);
+private:
+	bool _setSlave(uint8_t address); //return true on succes
+	bool _write(uint8_t address, uint8_t data); //return true on succes
+	bool _writeByte(uint8_t data);
+	int filedescriptor;
+	uint8_t currentTarget;
 };
-
-
 
 #endif
