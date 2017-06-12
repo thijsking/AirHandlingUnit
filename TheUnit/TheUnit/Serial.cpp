74bf9e97a285ba33897fb13a66838735631fbc19
thijsking <thijs.bril@outlook.com>
2017-06-12 14:00:49 +0200

    fixed it into c++

--- TheUnit/TheUnit/Serial.cpp
+++ TheUnit/TheUnit/Serial.cpp
@@ -6,7 +6,7 @@
 
 Serial::Serial() 
 {
-
+	Initialize();
 }
 
 Serial::~Serial()
@@ -32,10 +32,49 @@ bool Serial::Initialize()
 
 bool Serial::Write(uint8_t data, uint8_t address)
 {
+	unsigned char txBuffer[20];
+	unsigned char *p_tx_buffer;
 
+	p_tx_buffer = &txBuffer[0];
+	*p_tx_buffer++ = 0xFF;
+	*p_tx_buffer++ = address;
+	*p_tx_buffer++ = 0x86;
+	*p_tx_buffer++ = 0x00;
+	*p_tx_buffer++ = 0x00;
+	*p_tx_buffer++ = 0x00;
+	*p_tx_buffer++ = 0x00;
+	*p_tx_buffer++ = 0x00;
+	*p_tx_buffer++ = 0x79;
+	
+	if (uart0_filestream != -1)
+	{
+		int count = write(uart0_filestream, &txBuffer, sizeof(txBuffer) - 1);
+		if (count < 0)
+		{
+			std::cerr << "UART TX ERROR" << std::endl;
+			std::cerr << "data " << data << " ,adress " << address << std::endl;
+		}
+	}
 }
 
 bool Serial::Read(uint8_t* data, uint8_t length)
 {
+	bool doneReading = false;
 
+	while (!doneReading)
+	{
+		if (uart0_filestream != -1)
+		{
+			unsigned char rx_buffer[length];
+			int rx_lenght = read(uart0_filestream, (void*)rx_buffer, length);
+			if (rx_lenght > 0)
+			{
+				uint8_t high = (uint8_t)rx_buffer[2];
+				uint8_t low = (uint8_t)rx_buffer[3];
+				data[0] = high;
+				data[1] = low;
+				doneReading = true;
+			}
+		}
+	}
 }
