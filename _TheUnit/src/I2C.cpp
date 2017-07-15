9f044cdc9c09e2a38eeef0aeb63a761f9b69019f
rchard2scout <rchard2scout@users.noreply.github.com>
2017-07-15 16:55:01 +0200

    Finish documentation for I2C class

--- _TheUnit/src/I2C.cpp
+++ _TheUnit/src/I2C.cpp
@@ -19,6 +19,11 @@ I2C::~I2C(){
 	// reset multiplexer?
 }
 
+/**
+* To write a byte to the slave device, this function first checks if the
+* multiplexer is already set to the correct address. If it is, it immediately
+* calls `_write()`, otherwise it first calls `_setSlave()`
+*/
 bool I2C::Write(uint8_t data, uint8_t address){
 	if(address != currentTarget){
 		if(!_setSlave(address)){
@@ -29,6 +34,11 @@ bool I2C::Write(uint8_t data, uint8_t address){
 	return _write(SENSOR_ADDRESS, data);
 }
 
+/**
+* This function sets the multiplexer to the correct target. It first creates a
+* bitmask by using a bitshift. Next, it calls `_write`, and it updates the
+* `currentTarget`.
+*/
 bool I2C::_setSlave(uint8_t target){
 	int data = 1 << target; // create bitmask from target number
 	if(_write(MULTIPLEXER_ADDRESS, data)){
@@ -39,6 +49,11 @@ bool I2C::_setSlave(uint8_t target){
 	return false;
 }
 
+/**
+* This function configures the i2c device file with an i2c address, using the
+* `ioctl()` system call. Next, it calls `_writeByte()` to actually write the
+* byte.
+*/
 bool I2C::_write(uint8_t address, uint8_t data){
 	if((ioctl(filedescriptor, I2C_SLAVE, address)) < 0)
 	{
@@ -49,6 +64,11 @@ bool I2C::_write(uint8_t address, uint8_t data){
 	return _writeByte(data);
 }
 
+/**
+* This function writes the data byte to the i2c device file. It first creates a
+* two-character buffer containing the data byte and a null character, so that
+* the `write()` syscall has a null-terminated string to work with.
+*/
 bool I2C::_writeByte(uint8_t data){
 	uint8_t buffer[2] = {data, 0};
 	int length = 1;
@@ -61,6 +81,15 @@ bool I2C::_writeByte(uint8_t data){
 	return true;
 }
 
+/**
+* This function reads `length` bytes from the i2c device. It should always be
+* called after a call to `Write()`, so that a) the multiplexer is configured
+* correctly; and b) the slave device is actually about to send data.
+*
+* First, this function creates a temporary buffer of size `lenght + 1` to store
+* the data in. Next, it keeps trying to read data from the i2c device until it
+* is successful. Finally, it copies the read data into `buffer`.
+*/
 bool I2C::Read(uint8_t* buffer, uint8_t length){
 	uint8_t tmpbuffer[length + 1] = {0};
 	bool success = false;
