e886fa4fc88b9dad29f7243ee933e85944ee0eed
rchard2scout <rchard2scout@users.noreply.github.com>
2017-07-17 18:09:16 +0200

    Doxygen Humidity

--- _TheUnit/Include/Humidity.h
+++ _TheUnit/Include/Humidity.h
@@ -3,14 +3,42 @@
 
 #include "iSensor.h"
 
+/**
+* \brief This class controls a humidity sensor. The current implementation
+* assumes a Si7021 sensor connected to a TCA9548 i2c multiplexer. For more
+* information on the multiplexer, see the I2C class.
+*/
 class Humidity : public iSensor
 {
 public:
+/**
+* \brief Create a Humidity sensor object, and connect it to the Communication
+* device.
+*
+* @param communication A pointer to the communication object, expected to be an
+* I2C object.
+* @param address The data line of the multiplexer that this sensor is connected
+* to, should be a number from 0 to 7.
+*/
 	Humidity(iCommunication*, uint8_t);
+/**
+* \brief Destroys the Humidity sensor object.
+*/
 	virtual  ~Humidity();
+/**
+* \brief Get the current humidity as measured by the sensor
+*
+* @return The relative humidity in percent
+*/
 	virtual double GetValue();
 private:
+/**
+* \brief The communication device being used by this sensor
+*/
 	iCommunication* Communication;
+/**
+* \brief The data line of the multiplexer that this sensor is connected to
+*/
 	uint8_t Address;
 };
 
--- _TheUnit/src/Humidity.cpp
+++ _TheUnit/src/Humidity.cpp
@@ -7,10 +7,19 @@ Humidity::Humidity(iCommunication* comm, uint8_t address):
 
 Humidity::~Humidity(){}
 
+/**
+* This function gets the current relative humidity as measured by the sensor.
+* It first writes the byte `0xF5` to the sensor, which is the command for
+* "Measure Relative Humidity". This starts the measuring process of the sensor,
+* after which the data is read back into a buffer. This Read call is blocking
+* while the sensor is measuring, which should not take more than a few
+* milliseconds.
+*
+* The received two-byte buffer is turned into an integer, and the relative
+* humidity is calculated using the formula from the Si7021 datasheet.
+*/
 double Humidity::GetValue()
 {
-//TODO: use results of bool functions for error checking
-
 	//Measure RH, No-hold master mode
 	Communication->Write(0xF5, Address);
 
