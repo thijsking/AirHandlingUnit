162c5fa11d6e739e5e8dcd6ab93cb71b01419e35
rchard2scout <rchard2scout@users.noreply.github.com>
2017-07-17 17:54:56 +0200

    Doxygen comments for Temperature

--- _TheUnit/Include/Temperature.h
+++ _TheUnit/Include/Temperature.h
@@ -3,14 +3,42 @@
 
 #include "iSensor.h"
 
+/**
+* \brief This class controls a temperature sensor. The current implementation
+* assumes a Si7021 sensor connected to a TCA9548 i2c multiplexer. For more
+* information on the multiplexer, see the I2C class.
+*/
 class Temperature : public iSensor
 {
 public:
+/**
+* \brief Create a Temperature sensor object, and connect it to the
+* Communication device.
+*
+* @param communication A pointer to the communication object, expected to be an
+* I2C object.
+* @param address The data line of the multiplexer that this sensor is connected
+* to, should be a number from 0 to 7.
+*/
 	Temperature(iCommunication*, uint8_t);
+/**
+* \brief Destroys the Temperature sensor object.
+*/
 	virtual ~Temperature();
+/**
+* \brief Get the current temperature as measured by the sensor
+*
+* @return The temperature in degrees Celsius
+*/
 	virtual double GetValue();
 private:
+/**
+* \brief The communication device being used by this sensor
+*/
 	iCommunication* Communication;
+/**
+* \brief The data line of the multiplexer that this sensor is connected to
+*/
 	uint8_t Address;
 };
 
--- _TheUnit/src/Temperature.cpp
+++ _TheUnit/src/Temperature.cpp
@@ -9,6 +9,18 @@ Temperature::~Temperature()
 {
 }
 
+/**
+* This function gets the current temperature as measured by the sensor. It
+* first writes the byte `0xE0` to the sensor, which is the command for "Read
+* Temperature Value from Previous RH Measurement". Since both the temperature
+* and humidity should be read regularly, this measurement will probably not be
+* very outdated. If you want an up-to-date value instead, change this to
+* `0xF3`, or make sure the humidity is measured shortly before the temperature.
+*
+* Next, the data is read from the communication device into a two-byte buffer.
+* This buffer is turned into an integer, and the temperature is calculated
+* using the formula from the Si7021 datasheet.
+*/
 double Temperature::GetValue()
 {
 	Communication->Write(0xE0, Address);
